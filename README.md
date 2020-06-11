020db7f3969d01abaade97884ab34115539c252c
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-06-12 01:03:35 +0300

    nothing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # auth-server
 Express server that implements Basic Authentication, with signup and signin capabilities, using a Mongo database for storage.
+
+
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 'use strict';
 const express = require('express');
-const router = require('./auth/router.js');
-
+const notFound = require('./middleware/404.js');
+const serverError = require('./middleware/500.js');
 const app = express();
+const route = require('./auth/router');
+
+
 app.use(express.json());
-app.use(router);
+app.use(route);
+
+
+app.use('*', notFound);
+app.use(serverError);
+
 
 module.exports = {
   server: app,
